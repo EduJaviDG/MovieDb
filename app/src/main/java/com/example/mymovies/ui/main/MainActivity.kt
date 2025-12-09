646a40686cfa08967e0915c7075359d104799ed3
Eduardo J. Delgado <eduardo.delgado@wegow.es>
2025-12-09 14:00:37 +0100

    unregister permisssions in MainActivity

--- app/src/main/java/com/example/mymovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/ui/main/MainActivity.kt
@@ -130,6 +130,12 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        coarsePermission.unregister()
+        finePermission.unregister()
+    }
+
     private fun initRecycle() {
         movieAdapter = MovieAdapter()
         mLayoutManager = GridLayoutManager(this, 3)
