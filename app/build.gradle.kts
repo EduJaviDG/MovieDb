394acc223192bc8573327738a792524899be84f7
Eduardo J. Delgado <eduardo.delgado@wegow.es>
2025-12-09 14:04:19 +0100

    reformatted code in gradle.build module level

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         buildConfigField("String", "API_KEY", API_KEY)
         buildConfigField("String", "ACCESS_TOKEN", ACCESS_TOKEN)
     }
-    buildFeatures{
+    buildFeatures {
         buildConfig = true
     }
 
-    buildFeatures{
+    buildFeatures {
         viewBinding = true
     }
 
@@ -56,30 +56,35 @@ android {
 }
 
 dependencies {
-
-    //General
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+
     //Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.10.0")
     implementation("com.squareup.retrofit2:converter-gson:2.10.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
     //Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
     //Goggle
     implementation("com.google.android.gms:play-services-location:17.1.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.code.gson:gson:2.11.0")
+
     //Swipe Refresh
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("com.facebook.shimmer:shimmer:0.5.0")
+
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
     implementation("androidx.activity:activity-ktx:1.9.0")
+
     //Hilt
     implementation("com.google.dagger:hilt-android:2.46.1")
     kapt("com.google.dagger:hilt-compiler:2.46.1")
+
     //Room
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
@@ -87,9 +92,9 @@ dependencies {
 
     //Testing
     testImplementation("junit:junit:4.13.2")
-    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
-    testImplementation ("org.mockito:mockito-core:5.0.0")
-    testImplementation ("org.mockito.kotlin:mockito-kotlin:5.0.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+    testImplementation("org.mockito:mockito-core:5.0.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.0.0")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
